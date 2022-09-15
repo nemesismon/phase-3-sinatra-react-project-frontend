7442cab90ab67e3ed07870c7fcf9ff0de58f47a9
Michael <michael.w.kolb@gmail.com>
2022-09-15 13:52:57 -0400

    Created function components

--- my-app-frontend/src/Components/App.js
+++ my-app-frontend/src/Components/App.js
@@ -3,13 +3,10 @@ import Player from './Player';
 import Guess from './Guess';
 
 function App() {
-
-
+  //Updated App with creation of function components
 
   return (
     <div className="App">
-      {/* <header className="App-header">The Name Game
-      </header> */}
         <p>Fancy meeting you here. Would you like to play a game?
         </p>
         <Player />
@@ -19,23 +16,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-// {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         {/* <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a> */}
